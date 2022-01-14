6b7d781e4f67fd2d75c3f2eb0f100f69887a319c
Josh Peterson <jpeterson@esri.com>
2022-01-14 15:22:52 -0700

    add polygon to the map!

--- extension/source/LocatelyPopover/Map.js
+++ extension/source/LocatelyPopover/Map.js
@@ -2,9 +2,12 @@
 import React from "react";
 
 const Map = ({ place }) => {
+  console.log({ place });
   const xy = JSON.stringify(place.geometry);
-  const url = `https://esrips.github.io/locately/map?coords=${xy}`;
+  const city = place.features[0].attributes.AreaName;
+  const state = place.features[0].attributes.MajorSubdivisionAbbr;
 
+  const url = `https://esrips.github.io/locately/map?coords=${xy}&city=${city}&state=${state}`;
   return <iframe src={url}></iframe>;
 };
 
