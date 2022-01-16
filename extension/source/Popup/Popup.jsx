8f305838dd1c8269f521856763d0609ed32a4408
Cody Lawson <CLawson@esri.com>
2022-01-15 18:08:17 -0700

    Style update to settings page

--- extension/source/Popup/Popup.jsx
+++ extension/source/Popup/Popup.jsx
@@ -16,13 +16,13 @@ const Popup = () => {
     );
   }, []);
 
-  const settingChanged = (e) => {
+  const settingChanged = (value) => {
     chrome.storage.sync.set(
       {
-        dataCollection: e.target.value,
+        dataCollection: value,
       } // object
     );
-    setSettings({ ...settings, dataCollection: e.target.value });
+    setSettings({ ...settings, dataCollection: value });
   };
 
   return (
@@ -30,18 +30,58 @@ const Popup = () => {
       <h2>Locately Settings</h2>
       <form>
         <p>
-          <label htmlFor="dataCollection">Data Collection</label>
+          <h3 htmlFor="dataCollection">I'm interested in...</h3>
           <br />
-          <select
-            name="dataCollection"
-            id="dataCollection"
-            value={settings?.dataCollection}
-            onChange={settingChanged}
+          <label
+            for="Demographics"
+            className={`${
+              settings?.dataCollection === "Demographics" && "selected"
+            }`}
           >
-            <option value="Demographics">Demographics</option>
-            <option value="AtRisk">AtRisk</option>
-            <option value="Economy">Economy</option>
-          </select>
+            <input
+              type="radio"
+              id="Demographics"
+              name="dataCollection"
+              value="Demographics"
+              checked={settings?.dataCollection === "Demographics"}
+              onChange={(e) =>
+                e.target.checked && settingChanged("Demographics")
+              }
+            />
+            <i>👨‍👩‍👦‍👦</i>Demographics
+          </label>
+
+          <label
+            for="AtRisk"
+            className={`${settings?.dataCollection === "AtRisk" && "selected"}`}
+          >
+            <input
+              type="radio"
+              id="AtRisk"
+              name="dataCollection"
+              value="AtRisk"
+              checked={settings?.dataCollection === "AtRisk"}
+              onChange={(e) => e.target.checked && settingChanged("AtRisk")}
+            />
+            <i>🆘</i>At Risk
+          </label>
+
+          <label
+            for="Economy"
+            className={`${
+              settings?.dataCollection === "Economy" && "selected"
+            }`}
+          >
+            <input
+              type="radio"
+              id="Economy"
+              name="dataCollection"
+              value="Economy"
+              checked={settings?.dataCollection === "Economy"}
+              onChange={(e) => e.target.checked && settingChanged("Economy")}
+            />
+            <i>📈</i>Economy
+          </label>
         </p>
       </form>
     </section>
